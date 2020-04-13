fc64d860af28939acf39a0d456fbd8b303cd9cd8
Lr0514 <1146789385@qq.com>
2020-04-13 15:44:57 +0800

    测试demo

---
+++ src/test/java/com/lr/service/testdemo/TestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.service.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
